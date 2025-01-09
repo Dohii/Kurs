aed17aad8994fd3d466e075fa9d70647fdeb84ea
Hidajet Papucic <hidajetpap@gmail.com>
2025-01-09 14:47:21 +0100

    Fixed jsconfig

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import "@mantine/core/styles.css";
+import { createTheme, MantineProvider } from "@mantine/core";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
