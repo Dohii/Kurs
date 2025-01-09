f1f94653116535448635d74518b0696a5f1b02fa
hariskablar <125084282+hariskablar@users.noreply.github.com>
2025-01-09 19:18:18 +0100

    prvi pokusaj

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
 const supabaseClient = axios.create({
-  baseURL: process.env.REACT_APP_SUPABASE_URL + "/rest/v1",
+  baseURL: process.env.REACT_APP_SUPABASE_URL + '/rest/v1',
   headers: {
     apiKey: process.env.REACT_APP_SUPABASE_API_KEY,
     Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_API_KEY}`,
