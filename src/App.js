8eb7c50b5c657a6102d258ba41bd0ef708d0053e
hariskablar <125084282+hariskablar@users.noreply.github.com>
2025-01-25 20:56:34 +0100

    start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Container } from "@mantine/core";
-import { Header } from "./Shared/Layout/Header/Header";
-import { Footer } from "./Shared/Layout/Footer/Footer";
-import { Home } from "./Views/Home/Home";
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Container } from '@mantine/core';
+import { Header } from './Shared/Layout/Header/Header';
+import { Footer } from './Shared/Layout/Footer/Footer';
+import { Home } from './Views/Home/Home';
+import Firme from './Views/Firme/Firme';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/firme' element={<Firme />} />
         </Routes>
         <Footer />
       </BrowserRouter>
