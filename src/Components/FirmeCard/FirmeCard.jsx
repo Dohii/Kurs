e8afef60886c10e6f87b02cb16b920d56b531de7
hariskablar <125084282+hariskablar@users.noreply.github.com>
2025-01-28 17:56:44 +0100

    final

--- src/Components/FirmeCard/FirmeCard.jsx
+++ src/Components/FirmeCard/FirmeCard.jsx
@@ -1,5 +1,6 @@
 import { Card, Image, Text, Modal, Button, Group, Flex } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import classes from './FirmeCard.module.css';
 
 function FirmeCard({ firmaData }) {
   const { broj_uposlenika, created_at, naziv, opis, slika } = firmaData;
@@ -9,41 +10,46 @@ function FirmeCard({ firmaData }) {
   return (
     <>
       <Card shadow='sm' padding='xl' component='a'>
-        <Card.Section>
-          <Image src={slika} h={160} alt='No way!' />
+        <Card.Section className={classes.image} h='160'>
+          <Image src={slika} h='100%' alt='No way!' />
         </Card.Section>
 
         <Text fw={500} size='lg' mt='md'>
           {naziv}
         </Text>
 
-        <Button onClick={open}>Read more</Button>
+        <Button onClick={open}>Više detalja</Button>
       </Card>
       <Modal opened={opened} onClose={close}>
-        <Group>
-          <Text size='lg'>
-            <strong>Ime firme: </strong>
-          </Text>
-          <Text size='lg'>{naziv}</Text>
-        </Group>
-        <Flex direction='column'>
-          <Text size='lg'>
-            <strong>Opis poslovanja: </strong>
-          </Text>
-          <Text size='lg'>{opis}</Text>
-        </Flex>
-        <Group>
-          <Text size='lg'>
-            <strong>Broj uposlenika: </strong>
-          </Text>
-          <Text size='lg'>{broj_uposlenika}</Text>
-        </Group>
-        <Group>
-          <Text size='lg'>
-            <strong>Datum otvaranja: </strong>
-          </Text>
-          <Text size='lg'>{dateFormatted}</Text>
-        </Group>
+        <Card>
+          <Card.Section className={classes.image} h='160'>
+            <Image src={slika} h='100%' alt='No way!' />
+          </Card.Section>
+          <Group>
+            <Text size='lg'>
+              <strong>Ime firme: </strong>
+            </Text>
+            <Text size='lg'>{naziv}</Text>
+          </Group>
+          <Flex direction='column'>
+            <Text size='lg'>
+              <strong>Opis poslovanja: </strong>
+            </Text>
+            <Text size='lg'>{opis}</Text>
+          </Flex>
+          <Group>
+            <Text size='lg'>
+              <strong>Broj uposlenika: </strong>
+            </Text>
+            <Text size='lg'>{broj_uposlenika}</Text>
+          </Group>
+          <Group>
+            <Text size='lg'>
+              <strong>Datum otvaranja: </strong>
+            </Text>
+            <Text size='lg'>{dateFormatted}</Text>
+          </Group>
+        </Card>
       </Modal>
     </>
   );
